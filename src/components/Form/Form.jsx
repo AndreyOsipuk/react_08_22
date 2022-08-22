f0faac16f3570f0e60eea6a7e01d5ff99735c4d3
Andrey Osipuk <andrey.o@list.ru>
2022-08-22 22:47:49 +0300

    lesson 2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,34 @@
-import style from './Form.module.css'
+import { useCallback, useState, useRef, useEffect } from 'react';
+import { Child } from './Child';
 
 export const Form = () => {
-  const count = 1
-  const name = 'geekbrains'
-  return <form className={style.form}>
-    <p>Count: {count}</p>
-    <p>Name: {name}</p>
-    <button type="button">click</button>
-  </form>
-}
+  const [count, setCount] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const buttonEl = useRef(null);
+
+  const handleChangeCount = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, [setCount]);
+
+  // const memoizedValue = useMemo(() => computeExpensiveValue(a, b), [a, b]);
+
+  useEffect(() => {
+    console.log('ref', buttonEl);
+  }, []);
+
+  return (
+    <>
+      <h3>Parent component</h3>
+      <p>{count}</p>
+      <button ref={buttonEl} onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <br />
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? 'hide' : 'visible'}
+      </button>
+      <h3>Child component</h3>
+      {visible && <Child value={0} changeCount={handleChangeCount} />}
+    </>
+  );
+};
