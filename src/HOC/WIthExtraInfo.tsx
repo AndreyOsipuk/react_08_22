42f3df8c6243b7f79c2b107ac5d98c6c13a580d1
Andrey Osipuk <andrey.o@list.ru>
2023-08-05 12:20:38 +0400

    изменил типизацию хока

--- src/HOC/WIthExtraInfo.tsx
+++ src/HOC/WIthExtraInfo.tsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import { useState, ComponentType, FC } from "react";
 
 type ExtraInfoType = {
   extraInfo: string;
 };
 
-export function WithExtraInfo<P>(
-  WrappedComponent: React.ComponentType<P & ExtraInfoType>
-) {
-  const ComponentWithExtraInfo = (props: P) => {
-    const [extraInfo, setExtraInfo] = useState('info');
-    setExtraInfo('set new info');
+type HOCType = <P>(c: ComponentType<P & ExtraInfoType>) => FC<P>;
 
-    return <WrappedComponent {...props} extraInfo={extraInfo} />;
-  };
+export const WithExtraInfo2: HOCType = (Component) => (props) => {
+  const [extraInfo, setExtraInfo] = useState("info");
+  setExtraInfo("set new info");
 
-  return ComponentWithExtraInfo;
-}
+  return <Component {...props} extraInfo={extraInfo} />;
+};
+
+// export const WithExtraInfo = <P extends Record<string, any>>(
+//   WrappedComponent: React.ComponentType<P & ExtraInfoType>
+// ) => {
+//   const ComponentWithExtraInfo = (props: P) => {
+//     const [extraInfo, setExtraInfo] = useState("info");
+//     setExtraInfo("set new info");
+
+//     return <WrappedComponent {...props} extraInfo={extraInfo} />;
+//   };
+
+//   return ComponentWithExtraInfo;
+// };
